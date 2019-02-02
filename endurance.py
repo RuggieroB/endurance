eb619ae5b5815671d94ec14d9e12d5a65d68f0f8
EnricoRuggieroB <45107998+EnricoRuggieroB@users.noreply.github.com>
2019-02-02 11:56:28 +0000

    Algoritmo di Laboratorio di Programmazione. Alunno: Bucchianico Enrico Ruggiero, 4^Finf.

--- endurance.py
+++ endurance.py
@@ -1,19 +1,18 @@
 '''
 Algoritmo di Laboratorio di Programmazione. Alunno: Bucchianico Enrico Ruggiero, 4^Finf.
 
-Algoritmo "endurance.py": Algoritmo che dati in input la quantità di carburante (in galloni) di un aeroplano e il consumo orario (in galloni/h), 
-determina la durata massima di volo.
+Algoritmo "endurance.py": Algoritmo che dati in input la quantità di carburante (in galloni) di un aeroplano e 
+il consumo orario (in galloni/h), determina la durata massima di volo.
 '''
 
-print ("Endurance: \n Determina la durata massima di volo inserendo la quantita' di carburante (in galloni) ed il consumo orario (in galloni/h).  \n  \n  ")
+print ("Endurance: \n Determina la durata massima di volo inserendo la quantita' di carburante (in galloni) ed "\
+       "il consumo orario (in galloni/h).  \n  \n  ")
 carb = input ("Quantita' di carburante (galloni): ")
 carb = float (carb)
 cons = input ("Consumo orario (galloni/h): ")
 cons = float (cons)
 if carb < 0:
 	print (" \n ERRORE! La quantita' di carburante non puo' essere negativa!")
-	if cons <= 0:
-		print (" \n ERRORE! Il consumo orario deve essere maggiore di 0!")
 elif cons <= 0:
 	print (" \n ERRORE! Il consumo orario deve essere maggiore di 0!")
 else:
